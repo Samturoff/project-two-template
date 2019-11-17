1e0ca4185f6adfd30941dbb4c8cbc9c9e3890290
Samturoff <55993083+Samturoff@users.noreply.github.com>
2019-11-17 01:36:30 -0800

    social media buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,36 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
+ var placeholder = document.querySelectorAll('.placeholder'),
+    submit = document.querySelectorAll('.submit'),
+    input = document.getElementById('input');
 
+function clean(event) {
+  if(event.keyCode === 13) {
+    moveBtn();
+  }
+  placeholder[0].style.opacity = "0";
+  placeholder[0].innerHTML = "Check your inbox!";
+}
 
+input.addEventListener('keydown', function() {
+  placeholder[0].style.opacity = "0";
+});
 
+submit[0].addEventListener('click', function() {
+  moveBtn();
+});
 
-
-
-
-
-
-
-
+function moveBtn() {
+  submit[0].classList.add('move-btn');
+  placeholder[0].style.opacity = "0";
+  setTimeout(function() {
+    placeholder[0].innerHTML = "Check your inbox!"
+    submit[0].textContent = "Sweet";
+    placeholder[0].classList.add('placeholder-active');
+    input.value = "";
+  }, 500);
+} 
 
 
 
