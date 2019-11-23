9b61b242f86e68fe409e8814f7b6da4051c30143
Samturoff <55993083+Samturoff@users.noreply.github.com>
2019-11-23 00:26:28 -0800

    11/32/2019

--- scripts/script.js
+++ scripts/script.js
@@ -43,11 +43,13 @@ var close = document.getElementById("closebtn");
 var hamburger_background = document.getElementById("hamburger_background_id");
 var hideElem = document.getElementById("hide");
 var menulistid = document.getElementById("menulistid");
+var menu_link_container = document.getElementsByClassName("menu_link_container");
 
 
 
 $(document).ready(function() {
   $("#menulistid").css("visibility", "hidden");
+  $(".menu_link_container").css("z-index", "10");
 });
 //default to measure if/else from
 nav.style.height = "50px";
@@ -80,6 +82,7 @@ function navToggle() {
       $('#line_one_id').toggleClass('line1_active');
       $('#line_two_id').toggleClass('line2_active');      
       $('#line_three_id').toggleClass('line3_active');    
+      $(".menu_link_container").css("z-index", "10");
 
   } 
   //to open
@@ -98,8 +101,7 @@ function navToggle() {
       hamburger_background.style.backgroundColor = "rgba(133,51,45,0.95)";
       hamburger_background.style.zIndex = "10";
       $("#menulistid").css("visibility", "visible");
-
-
+      $(".menu_link_container").css("z-index", "-1");
   }
 
 };
