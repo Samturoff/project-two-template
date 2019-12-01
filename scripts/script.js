eb6974b06cf80bb304ccfd1cfc6aa85175702efb
Samturoff <55993083+Samturoff@users.noreply.github.com>
2019-11-30 21:10:44 -0800

    11/30/2019

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-// your script file for adding your own jquery
+// ---------------------------------------------------------------------------------------READY FUNCTION
+$(document).ready(function() {
+  $("#menulistid").css("visibility", "hidden");
+  $(".menu_breakfast_container").css("display", "block");
+});
+
+
 $(function() {
 // Your Code from here on down. Don't delete that line above!
  var placeholder = document.querySelectorAll('.placeholder'),
@@ -46,11 +52,6 @@ var menulistid = document.getElementById("menulistid");
 var menu_link_container = document.getElementsByClassName("menu_link_container");
 
 
-
-$(document).ready(function() {
-  $("#menulistid").css("visibility", "hidden");
-  $(".menu_link_container").css("z-index", "10");
-});
 //default to measure if/else from
 nav.style.height = "50px";
 main.style.marginTop = "50px";
@@ -113,59 +114,180 @@ $(function() {
   });
 });
 
-  //Change page
+  //---------------------------------------------------------------------------------------Change page
+
+
 var breakfast_page_id = document.getElementById("breakfast_page");
-var breakfast_menu_link = document.getElementByClassName("breakfast_menu_link");
+var brunch_page_id = document.getElementById("brunch_page");
+var lunch_page_id = document.getElementById("lunch_page");
+var dinner_page_id = document.getElementById("dinner_page");
+var dessert_page_id = document.getElementById("dessert_page");
 
+var breakfast_menu_line = document.getElementByClass("breakfast_menu_line");
 
-$(document).ready(function() {
-
-  $(".menu_breakfast_container").css("display", "none");
-    $(".breakfast_menu_link").css("display", "none");
-
-});
-//default to measure if/else from
-nav.style.height = "50px";
-main.style.marginTop = "50px";
-for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
-
-breakfast_page_id.addEventListener("click", function(){
-  var something = breakfast_page_id.children;
-  for (i = 0; i < something.length; i++){
-  something[i].classList.toggle("active");
-  }   
-});
 
 function breakfastToggle() {  
-  //to close
-  if (nav.style.height <= "275px") {
-  nav.style.height = "50px";
-  main.style.marginTop = "50px";
-  
-      var i = 0;
-      for (i = 0; i < menu.length; i++){
-  menu[i].style.transition="opacity .7s";      
-  menu[i].style.opacity="0.0";
-  menu[i].style.marginTop="100px";
-
-  };
-     
-    $(".menu_breakfast_container").css("display", "none");
-
-
-  } 
   //to open
-  else if (nav.style.height <= "50px") {
-  nav.style.height = "275px";
-  main.style.marginTop = "275px";
+  if (breakfast_page_id.style.color <= "black") {
+
       var i = 0;
       for (i = 0; i < menu.length; i++){
-  menu[i].style.opacity="1.0";
-  menu[i].style.marginTop="0px";
+
 
   };
      
-    $(".menu_breakfast_container").css("display", "block");
+  $(".menu_breakfast_container").css("display", "block");
+  $("#breakfast_page").css("color", "#85332D");
+  $('.breakfast_menu_line').addClass('menu_link_active');
+  $('.breakfast_menu_line').removeClass('menu_link_inactive');
+ 
+  $(".menu_brunch_container").css("display", "none");
+  $(".menu_lunch_container").css("display", "none");
+  $(".menu_dinner_container").css("display", "none");
+  $(".menu_dessert_container").css("display", "none");
+
+
+  $('.brunch_menu_line').addClass('menu_link_inactive');
+  $('.lunch_menu_line').addClass('menu_link_inactive');
+  $('.dinner_menu_line').addClass('menu_link_inactive');
+  $('.dessert_menu_line').addClass('menu_link_inactive');
+
+  $("#brunch_page").css("color", "black");
+  $("#lunch_page").css("color", "black");
+  $("#dinner_page").css("color", "black");
+  $("#dessert_page").css("color", "black");
+  }
+};
+
+
+function brunchToggle() {  
+  //to open
+  if (brunch_page_id.style.color <= "black") {
+
+      var i = 0;
+      for (i = 0; i < menu.length; i++){
+
+
+  };     
+  $(".menu_brunch_container").css("display", "block");
+  $("#brunch_page").css("color", "#85332D");
+  $('.brunch_menu_line').addClass('menu_link_active');
+  $('.brunch_menu_line').removeClass('menu_link_inactive');
+
+
+  $(".menu_breakfast_container").css("display", "none");
+  $(".menu_lunch_container").css("display", "none");
+  $(".menu_dinner_container").css("display", "none");
+  $(".menu_dessert_container").css("display", "none");
+
+  $('.breakfast_menu_line').addClass('menu_link_inactive');
+  $('.lunch_menu_line').addClass('menu_link_inactive');
+  $('.dinner_menu_line').addClass('menu_link_inactive');
+  $('.dessert_menu_line').addClass('menu_link_inactive');
+
+  $("#breakfast_page").css("color", "black");
+  $("#lunch_page").css("color", "black");
+  $("#dinner_page").css("color", "black");
+  $("#dessert_page").css("color", "black");  
+  }
+};
+
+  function lunchToggle() {  
+  //to open
+  if (lunch_page_id.style.color <= "black") {
+
+      var i = 0;
+      for (i = 0; i < menu.length; i++){
+
+
+  };     
+  $(".menu_lunch_container").css("display", "block");
+  $("#lunch_page").css("color", "#85332D");
+  $('.lunch_menu_line').addClass('menu_link_active');
+  $('.lunch_menu_line').removeClass('menu_link_inactive');
+
+
+  $(".menu_breakfast_container").css("display", "none");
+  $(".menu_brunch_container").css("display", "none");
+  $(".menu_dinner_container").css("display", "none");
+  $(".menu_dessert_container").css("display", "none");
+  
+  $('.breakfast_menu_line').addClass('menu_link_inactive');
+  $('.brunch_menu_line').addClass('menu_link_inactive');
+  $('.dinner_menu_line').addClass('menu_link_inactive');
+  $('.dessert_menu_line').addClass('menu_link_inactive');
+
+  $("#breakfast_page").css("color", "black");
+  $("#brunch_page").css("color", "black");
+  $("#dinner_page").css("color", "black");
+  $("#dessert_page").css("color", "black");  
+  }
+};
+
+
+  function dinnerToggle() {  
+  //to open
+  if (dinner_page_id.style.color <= "black") {
+
+      var i = 0;
+      for (i = 0; i < menu.length; i++){
+
+
+  };     
+  $(".menu_dinner_container").css("display", "block");
+  $("#dinner_page").css("color", "#85332D");
+  $('.dinner_menu_line').addClass('menu_link_active');
+  $('.dinner_menu_line').removeClass('menu_link_inactive');
+
+
+  $(".menu_breakfast_container").css("display", "none");
+  $(".menu_lunch_container").css("display", "none");
+  $(".menu_brunch_container").css("display", "none");
+  $(".menu_dessert_container").css("display", "none");
+  
+  $('.breakfast_menu_line').addClass('menu_link_inactive');
+  $('.lunch_menu_line').addClass('menu_link_inactive');
+  $('.brunch_menu_line').addClass('menu_link_inactive');
+  $('.dessert_menu_line').addClass('menu_link_inactive');
+
+  $("#breakfast_page").css("color", "black");
+  $("#brunch_page").css("color", "black");
+  $("#lunch_page").css("color", "black");
+  $("#dessert_page").css("color", "black");  
+  }
+};
+
+
+  function dessertToggle() {  
+  //to open
+  if (dessert_page_id.style.color <= "black") {
+
+      var i = 0;
+      for (i = 0; i < menu.length; i++){
+
+
+  };     
+  $(".menu_dessert_container").css("display", "block");
+  $("#dessert_page").css("color", "#85332D");
+  $('.dessert_menu_line').addClass('menu_link_active');
+  $('.dessert_menu_line').removeClass('menu_link_inactive');
+
+
+  $(".menu_breakfast_container").css("display", "none");
+  $(".menu_lunch_container").css("display", "none");
+  $(".menu_dinner_container").css("display", "none");
+  $(".menu_brunch_container").css("display", "none");
+  
+  $('.breakfast_menu_line').addClass('menu_link_inactive');
+  $('.lunch_menu_line').addClass('menu_link_inactive');
+  $('.dinner_menu_line').addClass('menu_link_inactive');
+  $('.brunch_menu_line').addClass('menu_link_inactive');
+
+  $("#breakfast_page").css("color", "black");
+  $("#brunch_page").css("color", "black");
+  $("#lunch_page").css("color", "black");
+  $("#dinner_page").css("color", "black");
+
 
   }
 };
